4539a89d27e23add91a6379ab9f01b467d8efed6
Paul Richardson <developer@photographerafoot.com>
2021-05-13 07:52:54 +1000

    CHG:  Set up table view

--- Countripedia/ViewController.swift
+++ Countripedia/ViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
 
+	// MARK:- Table View Data Source
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return 1
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+		return cell
+	}
 
 }
 
