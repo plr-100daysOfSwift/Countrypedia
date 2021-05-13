f802faefb004faaf1024d468b2cb1cf31d0e2ca0
Paul Richardson <developer@photographerafoot.com>
2021-05-13 14:37:09 +1000

    CHG: Country: Comparable
    compare names

--- Countripedia/Country.swift
+++ Countripedia/Country.swift
@@ -7,7 +7,11 @@
 
 import Foundation
 
-struct Country: Codable {
+struct Country: Codable, Comparable {
+
+	static func < (lhs: Country, rhs: Country) -> Bool {
+		return lhs.name < rhs.name
+	}
 
 	let name: String
 	let capitalCity: String
